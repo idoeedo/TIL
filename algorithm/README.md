8f32fda001f517d5cdcfb99e1728ea8c69a79bed
junexyz <commitbyido@gmail.com>
2025-08-01 17:46:33 +0900

    feat: add solution to an algorithm problem

--- algorithm/README.md
+++ algorithm/README.md
@@ -15,6 +15,7 @@
 
 | # | Platform | Problem | Code | Status | Notes |
 |:---:|--------|---------|:----:|:------:|:-----:|
+| 18 | LeetCode | [328. Odd Even Linked List](https://leetcode.com/problems/odd-even-linked-list/) | [.py](leetcode/0328.py) | ✅ |
 | 17 | LeetCode | [24. Swap Nodes in Pairs](https://leetcode.com/problems/swap-nodes-in-pairs/) | [.py](leetcode/0024.py) | ✅ |
 | 16 | LeetCode | [2. Add Two Numbers](https://leetcode.com/problems/add-two-numbers/) | [.py](leetcode/0002.py) | ✅ |
 | 15 | LeetCode | [206. Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/) | [.py](leetcode/0206.py) | ✅ |
---
+++ algorithm/leetcode/0328.py
@@ -0,0 +1,56 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class MyAnswer:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        odd_head = ListNode(1)
+        odd_node = odd_head
+
+        even_head = ListNode(2)
+        even_node = even_head
+
+        order = 1
+
+        while head:
+            if order % 2 == 1:
+                odd_node.next = ListNode(head.val)
+                odd_node = odd_node.next
+            else:
+                even_node.next = ListNode(head.val)
+                even_node = even_node.next
+
+            head = head.next
+            order += 1
+
+        odd_node.next = even_head.next
+
+        return odd_head.next
+
+
+class Solution1:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+
+        odd = head
+        odd_head = head
+
+        even = head.next
+        even_head = head.next
+
+        while even and even.next:
+            odd.next = odd.next.next
+            odd = odd.next
+
+            even.next = even.next.next
+            even = even.next
+
+        odd.next = even_head
+        return odd_head
